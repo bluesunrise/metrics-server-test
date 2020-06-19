42cdc1fd39a79a6643f42385d48562dffaad2018
David S Taylor <david@bluesunrise.com>
2020-06-19 16:52:13 -0700

    initial review of inventory and metrics apis

---
+++ go.mod
@@ -0,0 +1,10 @@
+module metrics-test
+
+go 1.13
+
+require (
+	github.com/davecgh/go-spew v1.1.1
+	k8s.io/apimachinery v0.18.3
+	k8s.io/client-go v0.18.3
+	k8s.io/metrics v0.18.3
+)
---
+++ main.go
@@ -0,0 +1,224 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/davecgh/go-spew/spew"
+	"k8s.io/client-go/kubernetes"
+	v1 "k8s.io/client-go/kubernetes/typed/core/v1"
+	"k8s.io/client-go/rest"
+	"os"
+
+	//"k8s.io/client-go/kubernetes"
+	//"k8s.io/client-go/rest"
+
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	metricsv "k8s.io/metrics/pkg/client/clientset/versioned"
+	v1beta1 "k8s.io/metrics/pkg/client/clientset/versioned/typed/metrics/v1beta1"
+)
+
+// https://github.com/kubernetes/client-go/tree/master/examples
+// https://github.com/kubernetes/client-go/tree/master/examples#configuration
+
+func main() {
+	kubernetesEndpoint := os.Getenv("METRICS_TEST_ENDPOINT")
+	if kubernetesEndpoint == "" {
+		kubernetesEndpoint = "localhost:8001"
+	}
+	config := rest.Config{
+		Host:                kubernetesEndpoint,
+		APIPath:             "",
+		ContentConfig:       rest.ContentConfig{},
+		Username:            "",
+		Password:            "",
+		BearerToken:         "",
+		BearerTokenFile:     "",
+		Impersonate:         rest.ImpersonationConfig{},
+		AuthProvider:        nil,
+		AuthConfigPersister: nil,
+		ExecProvider:        nil,
+		TLSClientConfig:     rest.TLSClientConfig{},
+		UserAgent:           "",
+		DisableCompression:  false,
+		Transport:           nil,
+		WrapTransport:       nil,
+		QPS:                 0,
+		Burst:               0,
+		RateLimiter:         nil,
+		Timeout:             0,
+		Dial:                nil,
+	}
+
+	kClientSet, err := kubernetes.NewForConfig(&config)
+	if err != nil {
+		panic(err.Error())
+	}
+	mClientSet, err := metricsv.NewForConfig(&config)
+	kapi := kClientSet.CoreV1()
+	mapi := mClientSet.MetricsV1beta1()
+	ctx := context.TODO()
+	GatherNodeInventory(kapi, ctx)
+	GatherPodInventory(kapi, ctx)
+	GatherNamespaceInventory(kapi, ctx)
+	GatherServiceInventory(kapi, ctx)
+	GatherNodeMetrics(mapi, ctx)
+	GatherPodMetrics(mapi, ctx)
+	GatherPersistentVolumes(kapi, ctx)
+	GatherEndPoints(kapi, ctx)
+	GatherReplicationControllers(kapi, ctx)
+	GatherConfigMaps(kapi, ctx)
+	GatherResourceQuotas(kapi, ctx)
+	GatherComponentStatuses(kapi, ctx)
+	GatherEvents(kapi, ctx)
+}
+
+func GatherNodeInventory(api v1.CoreV1Interface, context context.Context) {
+	fmt.Println("Node Inventory ....")
+	nodes, _ := api.Nodes().List(context, metav1.ListOptions{})
+	for _, node := range nodes.Items {
+		fmt.Printf("node = %s\n", node.Name)
+		for key, element := range node.Labels {
+			fmt.Printf("\tlabel = %s:%s\n", key, element)
+		}
+		fmt.Println("Node Capacity:")
+        spew.Dump(node.Status.Capacity)
+		fmt.Println("Node Allocatable:")
+		spew.Dump(node.Status.Allocatable)
+		fmt.Println("Conditions:")
+		spew.Dump(node.Status.Conditions)
+		fmt.Println("Addresses:")
+		spew.Dump(node.Status.Addresses)
+		fmt.Println("Volumes Attached:")
+		spew.Dump(node.Status.VolumesAttached)
+		fmt.Println("Volumes IN USE:")
+		spew.Dump(node.Status.VolumesAttached)
+		fmt.Println("Info:")
+		spew.Dump(node.Status.NodeInfo)
+		fmt.Println("Config:")
+		spew.Dump(node.Status.Config)
+		fmt.Println("Images:")
+		spew.Dump(node.Status.Images)
+		fmt.Println("Phase:")
+		spew.Dump(node.Status.Phase)
+
+	}
+}
+
+func GatherPodInventory(api v1.CoreV1Interface, context context.Context) {
+	// nodes
+	fmt.Println("Pod Inventory ....")
+	pods, _ := api.Pods("").List(context, metav1.ListOptions{})
+	for _, pod := range pods.Items {
+		fmt.Printf("pod = %s\n", pod.Name)
+		for key, element := range pod	.Labels {
+			fmt.Printf("\tlabel = %s:%s\n", key, element)
+		}
+		// TODO:
+		//fmt.Println("Conditions:")
+		//spew.Dump(pod.Status.Conditions)
+		//fmt.Println("Phase:")
+		//spew.Dump(pod.Status.Phase)
+	}
+}
+
+func GatherNamespaceInventory(api v1.CoreV1Interface, context context.Context) {
+	fmt.Println("Namespace Inventory ....")
+	namespaces, _ := api.Namespaces().List(context, metav1.ListOptions{})
+	for _, ns := range namespaces.Items {
+		fmt.Printf("namespace = %s\n", ns.Name)
+	}
+}
+
+func GatherServiceInventory(api v1.CoreV1Interface, context context.Context) {
+	fmt.Println("Service Inventory ....")
+	services, _ := api.Services("").List(context, metav1.ListOptions{})
+	for _, s := range services.Items {
+		fmt.Printf("service = %s\n", s.Name)
+	}
+}
+
+func GatherPersistentVolumes(api v1.CoreV1Interface, context context.Context) {
+	fmt.Println("Persistent Volumes Inventory ....")
+	volumes, _ := api.PersistentVolumes().List(context, metav1.ListOptions{})
+	for _, v := range volumes.Items {
+		fmt.Printf("volume = %s\n", v.Name)
+	}
+	claims, _ := api.PersistentVolumeClaims("").List(context, metav1.ListOptions{})
+	for _, v := range claims.Items {
+		fmt.Printf("volume claim = %s\n", v.Name)
+	}
+}
+
+func GatherEndPoints(api v1.CoreV1Interface, context context.Context) {
+	fmt.Println("End Points ....")
+	endpoints, _ := api.Endpoints("").List(context, metav1.ListOptions{})
+	for _, ep := range endpoints.Items {
+		fmt.Printf("endPoint = %s\n", ep.Name)
+	}
+}
+
+func GatherReplicationControllers(api v1.CoreV1Interface, context context.Context) {
+	fmt.Println("Replication Controllers ....")
+	replicationControllers, _ := api.ReplicationControllers("").List(context, metav1.ListOptions{})
+	for _, rp := range replicationControllers.Items {
+		fmt.Printf("replication Controller = %s\n", rp.Name)
+	}
+}
+
+func GatherConfigMaps(api v1.CoreV1Interface, context context.Context) {
+	fmt.Println("Config Maps ....")
+	configMaps, _ := api.ConfigMaps("").List(context, metav1.ListOptions{})
+	for _, cm := range configMaps.Items {
+		fmt.Printf("Config Map = %s\n", cm.Name)
+	}
+}
+
+func GatherEvents(api v1.CoreV1Interface, context context.Context) {
+	fmt.Println("Events ....")
+	events, _ := api.Events("").List(context, metav1.ListOptions{})
+	for _, e := range events.Items {
+		fmt.Printf("Events = %s\n", e.Name)
+	}
+}
+
+func GatherResourceQuotas(api v1.CoreV1Interface, context context.Context) {
+	fmt.Println("Resource Quotas ....")
+	quotas, _ := api.ResourceQuotas("").List(context, metav1.ListOptions{})
+	for _, q := range quotas.Items {
+		fmt.Printf("Quota = %s\n", q.Name)
+	}
+}
+
+func GatherComponentStatuses(api v1.CoreV1Interface, context context.Context) {
+	fmt.Println("Component Statuses ....")
+	statuses, _ := api.ComponentStatuses().List(context, metav1.ListOptions{})
+	for _, status := range statuses.Items {
+		fmt.Printf("Component Status = %s\n", status.Name)
+	}
+}
+
+func GatherNodeMetrics(api v1beta1.MetricsV1beta1Interface, context context.Context) {
+	nodes, _ := api.NodeMetricses().List(context, metav1.ListOptions{})
+	for _, node := range nodes.Items {
+		fmt.Println(node.Name, node.Namespace, node.ClusterName, node.Kind)
+		fmt.Println("\t", node.Usage.Cpu())
+		fmt.Println("\t", node.Usage.Memory())
+		fmt.Println("\t", node.Usage.Storage())
+		fmt.Println("\t", node.Usage.StorageEphemeral())
+	}
+}
+
+func GatherPodMetrics(api v1beta1.MetricsV1beta1Interface, context context.Context) {
+	pods, _ := api.PodMetricses("").List(context, metav1.ListOptions{})
+	for _, pod := range pods.Items {
+		fmt.Println(pod.Name, pod.Namespace, pod.Timestamp)
+		for _, container := range pod.Containers {
+			fmt.Println("\t" + container.Name)
+			fmt.Println("\t\t", container.Usage.Cpu())
+			fmt.Println("\t\t", container.Usage.Memory().Value())
+			fmt.Println("\t\t", container.Usage.Storage().Value())
+			fmt.Println("\t\t", container.Usage.StorageEphemeral().Value())
+		}
+	}
+}
+
